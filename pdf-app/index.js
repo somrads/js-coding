aee50beb8a189d3492abae2457f077079e3e367e
ssomrad <somradsharma@yahoo.com>
2024-01-31 15:32:54 +0100

    added onload

--- pdf-app/index.js
+++ pdf-app/index.js
@@ -1,23 +1,27 @@
-function showForm() {
-  let selectedMonth = document.getElementById("months").value;
-  let dynamicForm = document.getElementById("dynamicForm");
+window.onload = () => {
 
-  if (selectedMonth !== "") {
-    dynamicForm.style.display = "block";
-  } else {
-    dynamicForm.style.display = "none";
+  function showForm() {
+    let selectedMonth = document.getElementById("months").value;
+    let dynamicForm = document.getElementById("dynamicForm");
+  
+    if (selectedMonth !== "") {
+      dynamicForm.style.display = "block";
+    } else {
+      dynamicForm.style.display = "none";
+    }
   }
-}
-
-function submitForm() {
-  let selectedMonth = document.getElementById("months").value;
-  let eventName = document.getElementById("eventName").value;
-  let eventDate = document.getElementById("eventDate").value;
-
-  if (selectedMonth !== "" && eventName !== "" && eventDate !== "") {
-    // Handle form submission here (e.g., send data to the server)
-    alert("Form submitted!");
-  } else {
-    alert("Please fill in all required fields.");
+  
+  function submitForm() {
+    let selectedMonth = document.getElementById("months").value;
+    let eventName = document.getElementById("eventName").value;
+    let eventDate = document.getElementById("eventDate").value;
+  
+    if (selectedMonth !== "" && eventName !== "" && eventDate !== "") {
+      // Handle form submission here (e.g., send data to the server)
+      alert("Form submitted!");
+    } else {
+      alert("Please fill in all required fields.");
+    }
   }
+
 }
