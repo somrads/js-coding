de4e1b796752e4bc5aaf159ab6dcfeeff0b3d8a0
ssomrad <somradsharma@yahoo.com>
2024-01-31 15:00:11 +0100

    Added form and form validation

--- pdf-app/index.js
+++ pdf-app/index.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+function showForm() {
+  let selectedMonth = document.getElementById("months").value;
+  let dynamicForm = document.getElementById("dynamicForm");
+
+  if (selectedMonth !== "") {
+    dynamicForm.style.display = "block";
+  } else {
+    dynamicForm.style.display = "none";
+  }
+}
+
+function submitForm() {
+  let selectedMonth = document.getElementById('months').value;
+  let eventName = document.getElementById('eventName').value;
+  let eventDate = document.getElementById('eventDate').value;
+
+  if (selectedMonth !== "" && eventName !== "" && eventDate !== "") {
+    // Handle form submission here (e.g., send data to the server)
+    alert('Form submitted!');
+  } else {
+    alert('Please fill in all required fields.');
+  }
+}
