e903a4c66f808d6e7950341becda5905e4126e7b
ssomrad <somradsharma@yahoo.com>
2024-02-02 13:55:29 +0100

    Fetching api

--- pokemon-app/results/results.js
+++ pokemon-app/results/results.js
@@ -9,4 +9,30 @@ window.onload = function () {
 
   const descriptionDiv = document.getElementById("teamDescription");
   descriptionDiv.textContent = storedTeamDescription;
+
+  async function fetchPokemonData() {
+    // Create an AbortController and get its signal
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=151",
+        { signal }
+      );
+      const data = await response.json();
+
+      console.log(data.results);
+    
+    } catch (error) {
+      // Check if the error is due to the fetch being aborted
+      if (error.name === "AbortError") {
+        console.log("Fetch aborted");
+      } else {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    }
+  }
+  fetchPokemonData();
 };
+
